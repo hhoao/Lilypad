ab09d7c5c96ca1f8b72a28b37fe429414643dc03
shendrew <andrewsh190@gmail.com>
2026-04-27 20:24:10 -0400

    cleanup unused file util functions

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,23 +1,5 @@
 import GLib from 'gi://GLib';
 
-export function readJSON(filePath) {
-    let data = {};
-
-    try {
-        let [status, content] = GLib.file_get_contents(filePath);
-        data = JSON.parse(content);
-    } catch {
-        console.log(`File not found, creating new ${filePath}.`)
-    }
-    return data
-}
-
-
-export function saveJSON(filePath, data) {
-    let jsonData = JSON.stringify(data, null, '\t');
-    GLib.file_set_contents(filePath, jsonData);
-}
-
 export function getRoleName(role) {
     let roleName = role;
 
